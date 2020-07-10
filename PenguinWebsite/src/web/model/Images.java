8340b027066328489e389db1747219aa71ca47fe
Liu, Yang <liu.yang12@northeastern.edu>
2020-07-10 09:33:05 -0400

    add models, remove FrontEnd/ PenguinWebCode/

---
+++ PenguinWebsite/src/web/model/Images.java
@@ -0,0 +1,136 @@
+package web.model;
+
+import java.sql.Timestamp;
+
+public class Images {
+    protected int imageId;
+    protected String fileName;
+    protected String fileType;
+    protected Sites site;
+    protected String mediaLink;
+    protected Timestamp timestamp;
+    protected int width;
+    protected int height;
+    protected double longitude;
+    protected double latitude;
+    protected Cameras camera;
+
+    public Images(int imageId, String fileName, String fileType, Sites site,
+                  String mediaLink, Timestamp timestamp, int width, int height,
+                  double longitude, double latitude, Cameras camera) {
+        this.imageId = imageId;
+        this.fileName = fileName;
+        this.fileType = fileType;
+        this.site = site;
+        this.mediaLink = mediaLink;
+        this.timestamp = timestamp;
+        this.width = width;
+        this.height = height;
+        this.longitude = longitude;
+        this.latitude = latitude;
+        this.camera = camera;
+    }
+
+    public Images(String fileName, String fileType, Sites site,
+                  String mediaLink, Timestamp timestamp, int width, int height,
+                  double longitude, double latitude, Cameras camera) {
+        this.fileName = fileName;
+        this.fileType = fileType;
+        this.site = site;
+        this.mediaLink = mediaLink;
+        this.timestamp = timestamp;
+        this.width = width;
+        this.height = height;
+        this.longitude = longitude;
+        this.latitude = latitude;
+        this.camera = camera;
+    }
+
+    public int getImageId() {
+        return imageId;
+    }
+
+    public void setImageId(int imageId) {
+        this.imageId = imageId;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public String getFileType() {
+        return fileType;
+    }
+
+    public void setFileType(String fileType) {
+        this.fileType = fileType;
+    }
+
+    public Sites getSite() {
+        return site;
+    }
+
+    public void setSite(Sites site) {
+        this.site = site;
+    }
+
+    public String getMediaLink() {
+        return mediaLink;
+    }
+
+    public void setMediaLink(String mediaLink) {
+        this.mediaLink = mediaLink;
+    }
+
+    public Timestamp getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Timestamp timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public Cameras getCamera() {
+        return camera;
+    }
+
+    public void setCamera(Cameras camera) {
+        this.camera = camera;
+    }
+}
---
+++ PenguinWebsite/src/web/model/Sites.java
@@ -0,0 +1,48 @@
+package web.model;
+
+import java.sql.Date;
+
+public class Sites {
+    protected int siteId;
+    protected String name;
+    protected Date date;
+
+    public Sites(String name, Date date) {
+        this.name = name;
+        this.date = date;
+    }
+
+    public Sites(int siteId) {
+        this.siteId = siteId;
+    }
+
+    public Sites(int siteId, String name, Date date) {
+        this.siteId = siteId;
+        this.name = name;
+        this.date = date;
+    }
+
+    public int getSiteId() {
+        return siteId;
+    }
+
+    public void setSiteId(int siteId) {
+        this.siteId = siteId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+}
